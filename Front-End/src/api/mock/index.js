0882332178558a45e9e98129be5fe6166e8c6664
Francois-Adham <francoisadham6@gmail.com>
2021-05-19 03:41:57 +0200

    added axios for requests

--- Front-End/src/api/mock/index.js
+++ Front-End/src/api/mock/index.js
@@ -1,4 +1,5 @@
 import Course from './data/course.json';
+import Stream from './data/stream.json';
 
 const fetch = (mockData, time = 0) =>
   new Promise((resolve) => {
@@ -12,5 +13,10 @@ export default {
     console.log(id);
     const course = await fetch(Course, 1000);
     return course;
+  },
+
+  async fetchStream() {
+    const stream = await fetch(Stream, 1000);
+    return stream;
   },
 };
--- Front-End/src/views/ActivityStream/ActivityStream.vue
+++ Front-End/src/views/ActivityStream/ActivityStream.vue
@@ -1,26 +1,23 @@
 <template>
   <v-row justify="center">
-    <v-col cols="10">
-      <v-timeline
-        :dark="this.$store.state.dark"
-        align-top
-        :dense="$vuetify.breakpoint.smAndDown"
-      >
+    <v-col v-if="this.ready" cols="10">
+      <v-timeline dark align-top :dense="$vuetify.breakpoint.smAndDown">
         <v-timeline-item
-          v-for="(item, i) in events"
+          v-for="(item, i) in this.events"
           :key="i"
           :color="item.color"
           :icon="item.icon"
           fill-dot
         >
-          <v-card :color="item.color">
-            <v-card-title class="title"> Lorem Ipsum Dolor </v-card-title>
-            <v-card-text class="white text--primary">
+          <v-card class="elevation-20" :color="item.color">
+            <v-card-title class="title justify-center">
+              {{ item.title }}
+            </v-card-title>
+            <v-card-text
+              class="white font-weight-bold text--primary text-center"
+            >
               <p>
-                Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
-                scaevola imperdiet nec ut, sed euismod convenire principes at.
-                Est et nobis iisque percipit, an vim zril disputando
-                voluptatibus, vix an salutandi sententiae.
+                {{ item.due_date }}
               </p>
             </v-card-text>
           </v-card>
@@ -33,30 +30,48 @@
         <v-spacer></v-spacer>
       </v-row>
     </v-col>
+    <v-col v-if="!this.ready" cols="10">
+      <v-skeleton-loader v-bind="attrs" type="date-picker"></v-skeleton-loader>
+    </v-col>
   </v-row>
 </template>
 
 <script>
+import Client from 'api-client';
+
 export default {
   data: () => ({
-    events: [
-      {
-        color: 'red lighten-2',
-        icon: 'mdi-star',
-      },
-      {
-        color: 'purple darken-1',
-        icon: 'mdi-book-variant',
-      },
-      {
-        color: 'green lighten-1',
-        icon: 'mdi-airballoon',
-      },
-      {
-        color: 'indigo',
-        icon: 'mdi-buffer',
-      },
-    ],
+    events: [],
+    ready: false,
   }),
+  methods: {
+    async fetchStream() {
+      const stream = await Client.fetchStream();
+      for (const current_event of stream.data.events) {
+        if (current_event.type == 'quiz') {
+          this.events.push({
+            title: current_event.title,
+            due_date: current_event.date,
+            color: 'purple',
+            icon: 'mdi-comment-question-outline',
+          });
+        } else {
+          this.events.push({
+            title: current_event.title,
+            due_date: current_event.date,
+            color: 'blue',
+            icon: 'mdi-lead-pencil',
+          });
+        }
+      }
+      this.events.sort(function (x, y) {
+        return x.due_date - y.due_date;
+      });
+      this.ready = true;
+    },
+  },
+  async created() {
+    this.fetchStream();
+  },
 };
 </script>
