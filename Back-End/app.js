97cdac3b83c94fb0f5164d190073e8af3de8f5e5
AbdallahHussien4 <Abdalah.Abdelhameed98@eng-st.cu.edu.eg>
2021-06-07 18:21:45 +0200

    Solve chrome bug

--- Back-End/app.js
+++ Back-End/app.js
@@ -51,7 +51,7 @@ app.use((req,res,next)=>{
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
