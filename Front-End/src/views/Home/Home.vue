6a8bfb8cb44feffca88c1d09d252c5ceae131663
Francois-Adham <francoisadham6@gmail.com>
2021-06-09 00:14:31 +0200

    finished color design

--- Front-End/src/views/Home/Home.vue
+++ Front-End/src/views/Home/Home.vue
@@ -1,15 +1,13 @@
 <template>
-  <v-row style="width: 100%; height: 100%" class="ma-0">
-    <v-col cols="12" class="col pa-0">
-      <div class="bg mp-0 ma-0">
-        <v-row align="start" justify="start" style="width: 100%; height: 100%">
-          <v-col class="pa-10" cols="10">
-            <h1 class="welcome">
-              Welcome {{ this.$store.state.currentUser.username }} To Smarty
-            </h1>
-          </v-col>
-        </v-row>
-      </div>
+  <v-row align="center" justify="center" style="width: 100%; height: 100%">
+    <v-col class="pa-10 text-center" cols="10">
+      <h1
+        :class="$store.state.dark ? 'white--text' : 'grey--text text--darken-3'"
+        style="font-family: 'Sofia', sans-serif; font-size: 14vh"
+      >
+        Smarty
+      </h1>
+      <v-img :src="logo" class="logo" style="width: 65vh" />
     </v-col>
   </v-row>
 </template>
@@ -17,6 +15,15 @@
 <script>
 export default {
   name: 'Home',
+  computed: {
+    logo() {
+      if (this.$store.state.dark) {
+        return require('../../assets/logo_dark.svg');
+      } else {
+        return require('../../assets/logo_light.svg');
+      }
+    },
+  },
 };
 </script>
 
@@ -24,19 +31,14 @@ export default {
 @import url('https://fonts.googleapis.com/css?family=Sofia');
 
 .welcome {
-  color: #ffcd57;
-  font-family: 'Sofia', sans-serif;
-  font-size: 10vh;
 }
 .col {
   min-height: 100%;
 }
-.bg {
-  height: 100%;
-  display: flex;
-  align-items: center;
+
+.logo {
+  margin: auto;
+  align-self: center;
   align-content: center;
-  background-image: url('../../assets/home.jpg');
-  background-size: cover;
 }
 </style>
